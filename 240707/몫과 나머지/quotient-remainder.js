46bdb1c6251f27fadfe734481c8ff2895525e090
Dongho Shin <kakkiid@gmail.com>
2024-07-07 01:11:46 +0900

    Title: 몫과 나머지, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240707/몫과 나머지/quotient-remainder.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().split(' ').map(i => Number(i));
+
+console.log(parseInt(input[0] / input[1]));
+console.log(input[0] % input[1]);
