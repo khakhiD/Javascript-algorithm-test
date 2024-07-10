81a45543f74f702c62b8c92bead94c0b72c9eb21
Dongho Shin <kakkiid@gmail.com>
2024-07-11 01:12:08 +0900

    Title: 시험 통과 여부 확인하기, Time: 285ms, Memory: 14MB, Status: Passed - CodeTree

--- 240711/시험 통과 여부 확인하기/verify-test-passed.js
+++ 240711/시험 통과 여부 확인하기/verify-test-passed.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString());
 
-if (input > 80) {
+if (input >= 80) {
     console.log('pass');
 } else {
     const neededScore = 80 - input;
