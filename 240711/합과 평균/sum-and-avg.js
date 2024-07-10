b4b0298ba26b94ca0fbdd501ea6e2008d28a0124
Dongho Shin <kakkiid@gmail.com>
2024-07-11 00:42:11 +0900

    Title: 합과 평균, Time: 185ms, Memory: 47MB, Status: Passed - CodeTree

--- 240711/합과 평균/sum-and-avg.js
+++ 240711/합과 평균/sum-and-avg.js
@@ -4,4 +4,4 @@ let input = fs.readFileSync(0).toString().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log(a+b, (a+b)/input.length);
+console.log(a+b, ((a+b)/input.length).toFixed(1));
