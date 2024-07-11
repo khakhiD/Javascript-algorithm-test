b528f9c58251225f9d9badb2be11ce02f37e44bd
Dongho Shin <kakkiid@gmail.com>
2024-07-12 01:46:36 +0900

    Title: 총합 구하기, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240712/총합 구하기/find-the-total.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const [a, b] = input;
+let sum = 0;
+
+for (let i=a; i<=b; i++) {
+    if (i % 6 === 0 && i % 8 !== 0) sum += i;
+}
+
+console.log(sum);
