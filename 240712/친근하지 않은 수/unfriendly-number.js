6080200eed48938a6ca3336557b430692e9e6b2b
Dongho Shin <kakkiid@gmail.com>
2024-07-12 01:59:50 +0900

    Title: 친근하지 않은 수, Time: 219ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240712/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let input = Number(fs.readFileSync(0).toString().trim());
+
+const isDivided2or3or5 = (n) => n % 2 === 0 || n % 3 === 0 | n % 5 === 0;
+let count = 0;
+
+for (let i = 1; i <= input; i++) {
+    if (isDivided2or3or5(i)) {
+        count++;
+    }
+}
+
+console.log(count);
