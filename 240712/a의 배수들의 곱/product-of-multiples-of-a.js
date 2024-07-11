913a9cbe691c968a499450817ab63f49831d5b0e
Dongho Shin <kakkiid@gmail.com>
2024-07-12 01:49:58 +0900

    Title: a의 배수들의 곱, Time: 175ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240712/a의 배수들의 곱/product-of-multiples-of-a.js
+++ 240712/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
 const [a, b] = input;
-let result = 0;
+let result = 1;
 
 for (let i = a; i <= b; i++) {
     if (i % b === 0) result *= i;
