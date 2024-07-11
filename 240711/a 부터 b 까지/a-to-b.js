3bebfbecce956aed91d0125a0ff19e9483170cbd
Dongho Shin <kakkiid@gmail.com>
2024-07-11 22:26:34 +0900

    Title: a 부터 b 까지, Time: 504ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240711/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+let [a, b] = input;
+
+const calcaulate = (number) => {
+    if (number % 2 === 0) {
+        return number + 3;
+    } else {
+        return number * 2;
+    }
+}
+
+let result = [];
+
+
+for (let i = a; i <= b; calcaulate(i)) {
+    result.push(i);
+}
+
+console.log(result.join(' '));
