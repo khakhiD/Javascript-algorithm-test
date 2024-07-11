86f298bf7ac1026ecccb5d7ddf0704ab8131b61c
Dongho Shin <kakkiid@gmail.com>
2024-07-12 02:27:12 +0900

    Title: 조건에 부합하는 수, Time: 165ms, Memory: 15MB, Status: Passed - CodeTree

--- 240712/조건에 부합하는 수/number-that-meets-the-condition.js
+++ 240712/조건에 부합하는 수/number-that-meets-the-condition.js
@@ -3,7 +3,7 @@ let input = Number(fs.readFileSync(0).toString().trim());
 let result = [];
 
 for (let i = 1; i <= input; i++) {
-    if ((i % 2 === 0 && i % 4 !== 0) || (Math.floor(i / 8) % 2 === 0) || (i % 7 < 4)) {
+    if ((i % 2 === 0 && i % 4 !== 0) || (parseInt(i / 8) % 2 === 0) || (i % 7 < 4)) {
         continue;
     }
     result.push(i);
