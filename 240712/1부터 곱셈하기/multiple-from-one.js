9d3d5ad4211712992a708d889663d3c411c1a8cd
Dongho Shin <kakkiid@gmail.com>
2024-07-12 22:33:03 +0900

    Title: 1부터 곱셈하기, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240712/1부터 곱셈하기/multiple-from-one.js
+++ 240712/1부터 곱셈하기/multiple-from-one.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = Number(fs.readFileSync(0).toString().trim());
 let multiple = 1;
 
-for (let i=1; i<=input; i++) {
+for (let i=1; i<=10; i++) {
     multiple *= i;
     if (multiple > input) {
         console.log(i);
