99f1117a5a70d549d180e9915135cd2dadd2a0a0
Dongho Shin <kakkiid@gmail.com>
2024-07-22 21:50:14 +0900

    Title: 별표 출력하기 7, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240722/별표 출력하기 7/print-star-7.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+
+for (let i = 0; i < n; i++) {
+    let str = '';
+    for (let j = 0; j < i+1; j++) {
+        str += '* ';
+    }
+    console.log(str);
+};
