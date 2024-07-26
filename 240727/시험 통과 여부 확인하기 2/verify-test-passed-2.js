4a967571553312279e1a24cc26ae45f83fdffef6
Dongho Shin <kakkiid@gmail.com>
2024-07-27 01:25:09 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 240727/시험 통과 여부 확인하기 2/verify-test-passed-2.js
+++ 240727/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split("\n");
+
 let n = Number(input[0]);
 let scores = input.slice(1).map(i => i.split(' '));
-
-let sum = 0, avg = 0, cnt = 0;
+let cnt = 0;
 
 for (let i = 0; i < n; i++) {
-    sum = 0;
+    let sum = 0, avg = 0;
     scores[i].forEach(val => sum += Number(val))
     avg = (sum / scores[i].length)
 
