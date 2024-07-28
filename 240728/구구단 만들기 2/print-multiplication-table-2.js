8c987ea2313bd202a8d0f0f1d73c02a2dfc97682
Dongho Shin <kakkiid@gmail.com>
2024-07-28 22:07:09 +0900

    Title: 구구단 만들기 2, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240728/구구단 만들기 2/print-multiplication-table-2.js
+++ 240728/구구단 만들기 2/print-multiplication-table-2.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let str = '';
+let number = [], str = '';
 
 for (let i = b; i >= a; i--) {
     number.push(i);
