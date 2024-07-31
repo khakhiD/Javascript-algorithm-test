424449bed0cce370d0293899678503ea8b49e58f
Dongho Shin <kakkiid@gmail.com>
2024-08-01 01:18:24 +0900

    Title: 알파벳 출력하기, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240801/알파벳 출력하기/print-alphabet.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let line = '';
+let index = 'A'.charCodeAt();
+
+for (let i = 1; i <= n; i++) {
+    line = '';
+
+    for (let j = 1; j <= i; j++) {
+        line += String.fromCharCode(index);
+        
+        if (index > 'Z'.charCodeAt()) {
+            index = 'A'.charCodeAt();
+        } else {
+            index++;
+        }
+    }
+
+    console.log(line);
+}
