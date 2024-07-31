b5924b2d69330af69ed6790002622ac4f747bcb2
Dongho Shin <kakkiid@gmail.com>
2024-08-01 01:27:38 +0900

    Title: 알파벳 출력하기 2, Time: 188ms, Memory: 47MB, Status: Passed - CodeTree

--- 240801/알파벳 출력하기 2/print-alphabet-2.js
+++ 240801/알파벳 출력하기 2/print-alphabet-2.js
@@ -13,6 +13,7 @@ for (let i = 0; i < n; i++) {
 
     for (let j = 0; j < n - i; j++) {
         line += String.fromCharCode(index) + ' ';
+        
         if (String.fromCharCode(index) === 'Z') {
             index = 'A'.charCodeAt();
         } else {
